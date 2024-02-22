10b85065eb391a92a9f74e3390eb5536749b4711
Franz Kolo <francis.kolo@outlook.com>
2024-02-21 19:50:03 -0700

    just an example for hw

---
+++ test.py
@@ -0,0 +1,9 @@
+
+x = [1,2,3]
+y = x
+y += [4] #mutating the data set
+print(x) #prints [1, 2, 3, 4}
+x = [9,8,7] #reassigning x to this data set
+print(x) #prints [9, 8, 7]
+print(y) #prints [1, 2, 3, 4]
+
